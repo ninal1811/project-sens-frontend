1c7f96c18e62a138e27c8d41c518a0adcf1a7a29
sma9000 <sma9000@nyu.edu>
2026-03-04 19:15:13 -0500

    Adding Image Urls for Morocco's popular dishes

--- src/constants/imgUrls.js
+++ src/constants/imgUrls.js
@@ -1,5 +1,9 @@
 export const COUNTRY_IMAGE_URLS = {
-  MAR: 'https://koshercowboy.com/wp-content/uploads/2017/10/couscous-Photo-source-sahara-experience.jpg',
+  MAR: {
+    nat_dish: 'https://koshercowboy.com/wp-content/uploads/2017/10/couscous-Photo-source-sahara-experience.jpg',
+    pop_dish_1: 'https://verveculture.com/cdn/shop/articles/LambTagine3_copy.jpg?v=1620235939&width=1920',
+    pop_dish_2: 'https://api2.arabesquekitchen.com/images/recipe-chicken-pastilla-image-nk6cth/recipe-chicken-pastilla-image-nk6cth-optimized.webp?version=1683101082670'
+  },
   PHL: 'https://upload.wikimedia.org/wikipedia/commons/c/c9/Adobo_DSCF4391.jpg',
   CHN: 'https://www.seriouseats.com/thmb/5WEmtEV2Y37pU1joxKllpZ7uvDo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__20100910-duck-primary-43f6bb74c3b7490b9b6b99b643fd31f3.jpg',
   USA: 'https://www.sargento.com/assets/Uploads/Recipe/Image/burger_0__FocusFillWyIwLjAwIiwiMC4wMCIsODAwLDQ3OF0_CompressedW10.jpg',
