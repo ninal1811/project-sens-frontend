a44de23fb026c228c367c0ce050fcc40179f4137
sma9000 <sma9000@nyu.edu>
2026-03-10 17:30:54 -0400

    add Morocco restaurant images to cityImgs.js following constants folder split

--- src/constants/cityImgs.js
+++ src/constants/cityImgs.js
@@ -30,5 +30,53 @@ export const CITY_IMAGE_URLS = {
     Sylhet: {
       restaurant_name: "Panshi",
       image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/558985018.jpg?k=67c07e2be7b684706c594c43223f02484e9aa062f404856becc29ee09a7e78d1&o="
-    }
+    },
+    Tangier: {
+        restaurant_name: "Dar Harruch",
+        image: "https://lh3.googleusercontent.com/gps-cs-s/AHVAweqMoSaNthMizhkvQ55CagkZHtL6827ralEQV8cuNF4w5a2OhqNRleRjllHMdUSElq0lQqMyIiJuNMk5UYBf9OVwv0jIZaIpUCQr6raBw4qdpAgq6TZmkF_GYQY2PfJXaTisuA5EDLTx6Ak=s1360-w1360-h1020-rw"
+      },
+      Oujda: {
+        restaurant_name: "Restaurant Lamaalem",
+        image: "https://lh3.googleusercontent.com/gps-cs-s/AHVAweo-hvMgOMtie_XY19d5cDQa9_diouQ6CSTaT1tp0mbdwH6MLsArKvH4tAwFVlqd-p9GlBRZMeMHnP-wv9dGbfMaKjEcdV_v8SRfmuDsgXp36RX0xbBIkFwLksjr6L2KxzJ-Thna=s1360-w1360-h1020-rw"
+      },
+      Fez: {
+        restaurant_name: "El Forno",
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2e/8d/a8/56/caption.jpg?w=1100&h=-1&s=1"
+      },
+      Rabat: {
+        restaurant_name: "Dar Rbatia",
+        image: "https://lh3.googleusercontent.com/p/AF1QipPb3JJ3doQnzS-s9qT3vKvI07O69KSMTjkvkNAW=s1360-w1360-h1020-rw"
+      },
+      "Beni Mellal": {
+        restaurant_name: "O'darna",
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2d/7b/a2/4c/caption.jpg?w=1000&h=-1&s=1"
+      },
+      Casablanca: {
+        restaurant_name: "La Grande Table Marocaine",
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2b/87/78/08/la-grande-table-marocaine.jpg?w=1400&h=-1&s=1"
+      },
+      Marrakesh: {
+        restaurant_name: "Dar Cherifa",
+        image: "https://lh3.googleusercontent.com/p/AF1QipM7dYcAcdufUFLRIbkrq5OT5tsV1M7xxW_6B3_Q=s1360-w1360-h1020-rw"
+      },
+      Errachidia: {
+        restaurant_name: "Lune de Desert",
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/31/11/67/7c/caption.jpg?w=1400&h=800&s=1"
+      },
+      Agadir: {
+        restaurant_name: "La Barque Gourmande",
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/93/1b/71/caption.jpg?w=1400&h=-1&s=1"
+      },
+      Guelmim: {
+        restaurant_name: "Restaurant 33",
+        image: "https://lh3.googleusercontent.com/gps-cs-s/AHVAweopNQdDHmnLtQERDRtaZ6cPCNv36ZjX8PgxXauku4yEqg-mkyEdAT_DSHaWKt9zZmU6AYdKSQxa1tkYjIqpUSUD4mS6ghHjp4qbD3GHZepcBGVKXay5e1Z7RFmEvkKe8qxDdOtmJursl6ns=s1360-w1360-h1020-rw"
+      },
+      Laayoun: {
+        restaurant_name: "L'brunch",
+        image: "https://lh3.googleusercontent.com/p/AF1QipPXlsXU7VwlFYaUBWsAKKvNkgIafOS6P2GRHnga=s1360-w1360-h1020-rw"
+      },
+      Dakhla: {
+        restaurant_name: "Echarfaa Food",
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2e/fd/cb/e0/caption.jpg?w=700&h=-1&s=1"
+      },
   };
