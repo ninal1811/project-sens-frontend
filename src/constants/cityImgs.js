fb4885e4af0936cb766e6e3c90f10aa7e5635d66
sma9000 <sma9000@nyu.edu>
2026-03-10 16:25:51 -0400

    split image URLs into separate constants files

---
+++ src/constants/cityImgs.js
@@ -0,0 +1,34 @@
+export const CITY_IMAGE_URLS = {
+    Barishal: {
+      restaurant_name: "Zafran Restaurant Barishal",
+      image: "https://media-cdn.tripadvisor.com/media/photo-s/1b/63/e2/5c/an-elegant-upscale-multi.jpg"
+    },
+    Chattogram: {
+      restaurant_name: "Regalo",
+      image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/e0/15/78/the-wall.jpg?w=900&h=-1&s=1"
+    },
+    Dhaka: {
+      restaurant_name: "Amrit",
+      image: "https://img.p.mapq.st/?url=https%3A%2F%2Fmedia-cdn.tripadvisor.com%2Fmedia%2Fphoto-o%2F1c%2F92%2F7d%2F57%2Fsear.jpg&w=3840&q=75"
+    },
+    Khulna: {
+      restaurant_name: "Deshi Kitchen",
+      image: "https://media.evendo.com/locations-resized/RestaurantImages/360x263/668f930d-8921-48a6-b61f-8cb4a8238780"
+    },
+    Mymensingh: {
+      restaurant_name: "Sarinda",
+      image: "https://sarindagroup.com/wp-content/uploads/2025/08/IMG-20250817-WA0040.jpg"
+    },
+    Rajshahi: {
+      restaurant_name: "Kebab House",
+      image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2e/9f/cb/a0/caption.jpg?w=900&h=500&s=1"
+    },
+    Rangpur: {
+      restaurant_name: "Thikana",
+      image: "https://image-worker.mindtrip.ai/image-resize/format=webp,w=720/https:/images.mindtrip.ai/restaurants/27c8/0e21/5ad9/516a/070a/6a5d/9d07/3c00"
+    },  
+    Sylhet: {
+      restaurant_name: "Panshi",
+      image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/558985018.jpg?k=67c07e2be7b684706c594c43223f02484e9aa062f404856becc29ee09a7e78d1&o="
+    }
+  };
