cca6b6f0f4402940d8faacb3eecf4365381a1d6b
Erica Belcena <ericabelcena@gmail.com>
2026-03-25 22:17:06 -0400

    Resolve production build failure.

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useLocation } from "react-router";
-import "./Navbar.css";
+import "./NavBar.css";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
