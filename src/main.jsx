e039346230287405532f5331f1fd6e4ad045c39f
sma9000 <sma9000@nyu.edu>
2026-05-11 10:32:26 -0400

    Reverting change

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "leaflet/dist/leaflet.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/project-sens-frontend'>
       <App/>
     </BrowserRouter>
   </StrictMode>,
